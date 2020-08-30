e565888f25bc4170d168b811e0b46fe84c66e205
Alim Nevilov <52937912+nevilov@users.noreply.github.com>
2020-08-30 17:20:51 +0300

    V-0.1

--- app/src/main/java/com/example/encyclopedia/MainActivity.java
+++ app/src/main/java/com/example/encyclopedia/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.encyclopedia;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupMenu();
+    }
+    private void setupMenu() {
+        FragmentManager fm = getSupportFragmentManager();
+        MenuFragmentList mMenuFragment = (MenuFragmentList) fm.findFragmentById(R.id.id_container_menu);
+        if (mMenuFragment == null) {
+            mMenuFragment = new MenuFragmentList();
+            fm.beginTransaction().add(R.id.id_container_menu, mMenuFragment).commit();
+        }
     }
 }
