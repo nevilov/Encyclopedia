374d582efba11cd782f842d8551b1e9b5e1a5689
Alim Nevilov <52937912+nevilov@users.noreply.github.com>
2020-09-02 15:01:57 +0300

    V-0.3

--- app/src/main/java/com/example/encyclopedia/MenuFragmentList.java
+++ app/src/main/java/com/example/encyclopedia/MenuFragmentList.java
@@ -1,22 +1,31 @@
 package com.example.encyclopedia;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+
+import android.text.Spannable;
+import android.text.SpannableString;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SubMenu;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-
+import com.example.encyclopedia.CustomTypeFaceSpan;
+import com.example.encyclopedia.R;
 import com.google.android.material.navigation.NavigationView;
 
 
 
 public class MenuFragmentList extends Fragment {
+
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -33,14 +42,39 @@ public class MenuFragmentList extends Fragment {
         vNavigation.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                Toast.makeText(getActivity(),menuItem.getTitle(),Toast.LENGTH_SHORT).show();
+                Toast.makeText(getActivity(), menuItem.getTitle(), Toast.LENGTH_SHORT).show();
                 return false;
             }
-        }) ;
+        });
 
-        return  view ;
+        Menu m = vNavigation.getMenu();
+        for (int i=0;i<m.size();i++) {
+            MenuItem mi = m.getItem(i);
+
+            //for aapplying a font to subMenu ...
+            SubMenu subMenu = mi.getSubMenu();
+            if (subMenu!=null && subMenu.size() > 0 ) {
+                for (int j=0; j <subMenu.size();j++) {
+                    MenuItem subMenuItem = subMenu.getItem(j);
+                    applyFontToMenuItem(subMenuItem);
+                }
+            }
+
+            //the method we have create in activity
+            applyFontToMenuItem(mi);
+        }
+
+
+
+        return view;
     }
 
-
-
+    private void applyFontToMenuItem(MenuItem mi) {
+        if (getActivity() != null) {
+            Typeface font = Typeface.createFromAsset(getActivity().getAssets(), getString(R.string.font));
+            SpannableString mNewTitle = new SpannableString(mi.getTitle());
+            mNewTitle.setSpan(new CustomTypeFaceSpan("", font), 0, mNewTitle.length(), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+            mi.setTitle(mNewTitle);
+        }
+    }
 }
